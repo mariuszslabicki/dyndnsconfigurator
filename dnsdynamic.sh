cc4e9899936f76f316194c479b2060b408ec3870
Mariusz Slabicki <root@raspberrypi.(none)>
2013-11-17 11:07:30 +0000

    Small update - now it works with freedns.afraid.org

--- dnsdynamic.sh
+++ dnsdynamic.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-#check if the file with old ip exist
+
+UPDATEURL="http://freedns.afraid.org/dynamic/update.php?YOURAPIKEYHERE"
+
+#check if the file with ip exists
 if [ -e ip_last.tmp ]
 then 
 	#if yes, read old ip addr
@@ -20,12 +23,6 @@ fi
 
 #save external ip to file
 echo $EXTERNAL_IP > ip_last.tmp
-#username used in www.dnsdynamic.org -- @ changed for %40
-USER="username%40domain.net"
-#password used
-PASSWD="simplepasswd"
-#hostname configured in www.dnsdynamic.org, below is just example
-HOSTNAME='exampledomain.flashserv.net'
-#whole request
-REQUEST="https://$USER:$PASSWD@www.dnsdynamic.org/api/?hostname=$HOSTNAME&myip=$EXTERNAL_IP"
-wget -qO /dev/null $REQUEST
+
+#make a request for update
+wget -q -O /dev/null $UPDATEURL
